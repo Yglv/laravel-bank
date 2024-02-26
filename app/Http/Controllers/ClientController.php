0d34d85be1143cc33c4f982c193a0f94eda86630
Golovchenko Yegor <78694320+Yglv@users.noreply.github.com>
2024-02-26 10:30:06 +0300

    feat: add controller for clients

---
+++ app/Http/Controllers/ClientController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class ClientController extends Controller
+{
+    public function index()
+    {
+
+    }
+
+    public function store()
+    {
+
+    }
+
+    public function create()
+    {
+
+    }
+
+    public function show()
+    {
+
+    }
+
+    public function update()
+    {
+
+    }
+
+    public function destroy()
+    {
+
+    }
+
+}
